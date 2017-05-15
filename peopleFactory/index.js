a77437a06181c1c0ab5117524df4860d8fa474b7
AdamKogut <akogut@purdue.edu>
2017-05-15 19:50:51 -0400

    Add everything to git for the fifth(?) time

---
+++ peopleFactory/index.js
@@ -0,0 +1,36 @@
+const personForm=document.querySelector('form')
+
+const renderColor = (hairColor)=>{
+    const colorDiv=document.createElement('div')
+    //colorDiv.innerHTML=hairColor
+    colorDiv.style.backgroundColor=hairColor
+    colorDiv.style.height='50px'
+    colorDiv.style.width='100px'
+    return colorDiv
+}
+
+const handleSubmit= (ev) => {
+    ev.preventDefault()
+    const form=ev.target
+    const details=document.querySelector('.details')
+
+    const personName=form.personName.value
+    const hairColor=form.hairColor.value
+    const age=form.age.value
+    const birthplace=form.birthplace.value
+
+    const em=document.createElement('em')
+    em.innerHTML=personName
+
+    const colorDiv=renderColor(hairColor)
+
+    details.innerHTML=`
+        <ul>
+            <li>Name: ${personName}</li>
+            <li>Hair Color: ${colorDiv.outerHTML}</li>
+            <li>Age: ${age}</li>
+            <li>Birthplace ${birthplace}</li>
+        </ul>`
+}
+
+personForm.addEventListener('submit',handleSubmit)
