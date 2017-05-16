712d39cc9bfb63db1b22ebb5cae9ff61b21685a0
AdamKogut <akogut@purdue.edu>
2017-05-16 10:03:41 -0400

    Remove hard coding for names(except hair color)

--- peopleFactory/index.js
+++ peopleFactory/index.js
@@ -9,9 +9,22 @@ const renderColor = (hairColor)=>{
     return colorDiv
 }
 
-const elementAdding=(arr,doc,listhead)=>{
-    for(let i=0;i<arr.length;i++){
-        listhead.appendChild(arr[i])
+const elementAdding=(arr,doc,listhead,colorPatch)=>{
+    let i=0
+    for(;i<arr.length,arr[i].name!='hairColor';i++){
+        const em=document.createElement('li')
+        em.textContent=arr[i].name+': '+arr[i].value
+        listhead.appendChild(em)
+    }
+    const em=document.createElement('li')
+    em.textContent='hair color:'
+    listhead.appendChild(em)
+    listhead.appendChild(colorPatch)
+    i++
+    for(;i<arr.length-1;i++){
+        const ema=document.createElement('li')
+        ema.textContent=arr[i].name+': '+arr[i].value
+        listhead.appendChild(ema)
     }
     doc.appendChild(listhead)
 }
@@ -21,31 +34,34 @@ const handleSubmit= (ev) => {
     const form=ev.target
     const details=document.querySelector('.details')
 
-    const personName=form.personName.value
+    const whole=document.createElement('ul')
+    /*for(let i=0;i<form.length;i++){
+        
+    }*/
+    //const personName=form.personName.value
     const hairColor=form.hairColor.value
-    const age=form.age.value
+    /*const age=form.age.value
     const birthplace=form.birthplace.value
 
-    const whole=document.createElement('ul')
+    
 
     const em=document.createElement('li')
     em.textContent='Name: '+ personName
 
     const colwords=document.createElement('li')
-    colwords.textContent='Hair Color:'
+    colwords.textContent='Hair Color:'*/
 
     const colorDiv=renderColor(hairColor)
 
-    const ageWords=document.createElement('li')
+    /*const ageWords=document.createElement('li')
     ageWords.textContent='Age: '+age
 
     const birthWords=document.createElement('li')
-    birthWords.textContent='Birthplace: '+birthplace
+    birthWords.textContent='Birthplace: '+birthplace*/
 
     details.innerHTML=''
 
-    const arr=[em,colwords,colorDiv,ageWords,birthWords]
-    elementAdding(arr,details,whole)
+    elementAdding(form,details,whole,colorDiv)
 
     /*details.innerHTML=`
         <ul>
