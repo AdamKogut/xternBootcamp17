c68627e3219027c9b43b8cb23b4f3495d947e648
AdamKogut <akogut@purdue.edu>
2017-05-17 15:10:48 -0400

    Create a student object with a name and id

--- rosterApp/classEx/js/app.js
+++ rosterApp/classEx/js/app.js
@@ -1,16 +1,28 @@
 $(document).foundation()
 
 const megaroster={
+    max:0,
+
     init(){
+        //can put this.max=0 here to replace max above
         document
             .querySelector('#new-student')
-            .addEventListener('submit',this.addStudent)
+            .addEventListener('submit',this.addStudent.bind(this))
     },
 
     addStudent(ev){
         ev.preventDefault()
-        const studentName=ev.target.studentName.value
-        console.log(studentName)
+        const f=ev.target
+        const student={
+            id: ++this.max,
+            name:f.studentName.value,
+        }
+        
+        this.buildListItem(student)
+    },
+
+    buildListItem(student){
+        console.log(student)
     },
 }
 megaroster.init()
